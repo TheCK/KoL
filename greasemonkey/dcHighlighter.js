ddb5dcdff5f746f8dfc4201d1d0254ed173b5ee9
TheCK <karsten_knuth@gmx.net>
2014-07-11 15:27:14 +0200

    added highlight in mall

--- greasemonkey/dcHighlighter.js
+++ greasemonkey/dcHighlighter.js
@@ -5,6 +5,7 @@
 // @include     http://www.kingdomofloathing.com/managecollection.php
 // @include     http://www.kingdomofloathing.com/fight.php
 // @include     http://www.kingdomofloathing.com/closet.php*
+// @include     http://www.kingdomofloathing.com/mall.php*
 // @version     1
 // @grant       GM_setValue
 // @grant       GM_getValue
@@ -20,13 +21,14 @@ if (window.location.pathname == "/managecollection.php")
     {
         GM_deleteValue(key);
     }
-
+    
     var selects = document.getElementsByName("whichitem1");
-
-    if (typeof selects[1] != "undefined")
+    
+    if (typeof selects[1] != "undefined") 
     {
         var dcItems = selects[1].options;
-
+        console.log(dcItems.length)
+        
         for (dcItem in dcItems)
         {
             GM_setValue(dcItems[dcItem].value, true);
@@ -42,7 +44,7 @@ if (window.location.pathname == "/inventory.php" || window.location.pathname == 
         {
             var invItem = invItems[invIndex];
             invItem.style.background = "red";
-
+            
             if(GM_getValue(invItem.id.slice(2), false))
             {
                 invItem.style.background = "green";
@@ -57,12 +59,29 @@ if (window.location.pathname == "/fight.php")
     for (invIndex in invItems) {
         var newItem = invItems[invIndex];
         newItem.style.background = "red";
-
+        
         var regEx = /id=(\d*)/g;
         var match = newItem.getAttribute("rel").match(regEx);
         if(GM_getValue(match[0].slice(3), false))
         {
             newItem.style.background = "green";
+        }
+    }
+}
+
+if (window.location.pathname == "/mall.php")
+{
+    mallTables = document.getElementsByClassName("itemtable");
+    console.log(mallTables);
+    for (mallTable in mallTables) {
+        var newTable = mallTables[mallTable];
+        newTable.style.background = "red";
+        
+        var match = newTable.childNodes[0].childNodes[0].getAttribute("id");
+        console.log(match.slice(5));
+        if(GM_getValue(match.slice(5), false))
+        {
+            newTable.style.background = "green";
         }
     }
 }
