eb3135fe7ae3e9c29e87cf66a43245a0823573cd
TheCK <karsten_knuth@gmx.net>
2014-07-11 15:28:20 +0200

    replaced by correct version

--- greasemonkey/mall.js
+++ greasemonkey/mall.js
@@ -1,86 +1,140 @@
 // ==UserScript==
-// @name        CK's Display Case Highlighter
-// @namespace   org.ck.kol
+// @name        Ck's Mall Price Script
+// @namespace   org.ck,kol
 // @include     http://www.kingdomofloathing.com/inventory.php*
-// @include     http://www.kingdomofloathing.com/managecollection.php
-// @include     http://www.kingdomofloathing.com/fight.php
 // @include     http://www.kingdomofloathing.com/closet.php*
-// @include     http://www.kingdomofloathing.com/mall.php*
-// @version     1
+// @version     1.1
+// @grant       GM_xmlhttpRequest
 // @grant       GM_setValue
 // @grant       GM_getValue
 // @grant       GM_listValues
 // @grant       GM_deleteValue
 // ==/UserScript==
 
+var tinyTexts = [];
+var ids = [];
+var sum = 0;
 
-if (window.location.pathname == "/managecollection.php")
+function setPrice(searchTerm, price)
+{
+    priceNode = document.createTextNode(price);
+    tinyTexts[searchTerm].appendChild(priceNode);
+}
+
+function deletePrices()
 {
     keys = GM_listValues();
     for (i=0, key=null; key=keys[i]; i++)
     {
         GM_deleteValue(key);
     }
+}
 
-    var selects = document.getElementsByName("whichitem1");
-
-    if (typeof selects[1] != "undefined")
+if (window.location.pathname == "/inventory.php" || window.location.pathname == "/closet.php")
+{
+    invItems = document.getElementsByClassName("item");
+    for (invIndex in invItems)
     {
-        var dcItems = selects[1].options;
-
-        for (dcItem in dcItems)
+    
+        if (!isNaN(invIndex))
         {
-            GM_setValue(dcItems[dcItem].value, true);
+            invItem = invItems[invIndex];
+            id = invItem.id.slice(2);
+    
+            pr = GM_getValue(id, false);
+            
+            bs = invItem.querySelectorAll("[class=ircm]");
+            search = bs[0].firstChild.nodeValue;
+            tinyTexts[search] = bs[0].parentNode.childNodes[3];
+            ids[search] = id;
+
+            if (pr === false)
+            {
+                GM_xmlhttpRequest({
+                    method: "GET",
+                    url: "http://www.kingdomofloathing.com/mall.php?pudnuggler=" + encodeURIComponent("\"" + search + "\""),
+                    onload: function(response)
+                    {
+                        doc = document.implementation.createHTMLDocument('');
+                        doc.documentElement.innerHTML = response.responseText;
+                        
+                        searchTerm = doc.getElementById("pudnuggler").getAttribute("value");
+                        
+                        priceTags = doc.getElementById("searchresults").querySelectorAll("[class='small price']");
+
+                        which = 4;
+                        while (typeof priceTags[which] == "undefined" && which > 0)
+                        {
+                            --which;
+                        }
+                        if (typeof priceTags[which] != "undefined")
+                        {
+                            
+                            price = priceTags[which].firstChild.innerHTML.replace("&nbsp;", " ");
+                            price += " (" + (which+1) + ".)"
+                            
+                            if (which != 0 && typeof priceTags[0] != "undefined")
+                            {
+                                price += " / " + priceTags[0].firstChild.innerHTML.replace("&nbsp;", " ") + " (1.)";
+                            }
+                            
+                            GM_setValue(ids[searchTerm.slice(1, -1)], price) ;
+                            setPrice(searchTerm.slice(1, -1), price);
+                        }
+                        else
+                        {
+                            GM_setValue(ids[searchTerm.slice(1, -1)], "untradable");
+                            setPrice(searchTerm.slice(1, -1), "untradable");
+                        }
+                    }
+                });
+            }
+            else
+            {
+                times = 1;
+                
+                if (bs[0].parentNode.childNodes[2].hasChildNodes())
+                {
+                   times = parseInt(bs[0].parentNode.childNodes[2].childNodes[0].nodeValue.replace( /\D/g, ''));
+                }
+                
+                val = GM_getValue(id, false);
+                priceLine = val.match( /[\d,]+/g, '');
+                
+                if (priceLine != null)
+                {
+                    intVal = parseInt(priceLine[0].replace( /\D/g, ''));
+                    
+                    if(!isNaN(intVal))
+                    {
+                       sum += times * intVal;
+                    }
+                }
+
+                setPrice(search, GM_getValue(id, false));
+            }
         }
     }
 }
 
 if (window.location.pathname == "/inventory.php" || window.location.pathname == "/closet.php")
 {
-    invItems = document.getElementsByClassName("item");
-    for (invIndex in invItems) {
-        if (!isNaN(invIndex))
-        {
-            var invItem = invItems[invIndex];
-            invItem.style.background = "red";
-
-            if(GM_getValue(invItem.id.slice(2), false))
-            {
-                invItem.style.background = "green";
-            }
-        }
-    }
-}
-
-if (window.location.pathname == "/fight.php")
-{
-    invItems = document.getElementsByClassName("item");
-    for (invIndex in invItems) {
-        var newItem = invItems[invIndex];
-        newItem.style.background = "red";
-
-        var regEx = /id=(\d*)/g;
-        var match = newItem.getAttribute("rel").match(regEx);
-        if(GM_getValue(match[0].slice(3), false))
-        {
-            newItem.style.background = "green";
-        }
-    }
-}
-
-if (window.location.pathname == "/mall.php")
-{
-    mallTables = document.getElementsByClassName("itemtable");
-    console.log(mallTables);
-    for (mallTable in mallTables) {
-        var newTable = mallTables[mallTable];
-        newTable.style.background = "red";
-
-        var match = newTable.childNodes[0].childNodes[0].getAttribute("id");
-        console.log(match.slice(5));
-        if(GM_getValue(match.slice(5), false))
-        {
-            newTable.style.background = "green";
-        }
-    }
+    center = document.getElementsByTagName('center')[0].childNodes[1].firstChild.childNodes[1].firstChild.firstChild.firstChild.firstChild.firstChild.firstChild.firstChild;
+    
+    open = document.createTextNode("  [");
+    center.appendChild(open);
+    
+    a = document.createElement('a');
+    linkText = document.createTextNode("refresh mall prices");
+    a.appendChild(linkText);
+    a.href = "#";
+    a.addEventListener('click', deletePrices, false);
+    
+    center.appendChild(a);
+    
+    close = document.createTextNode("]");
+    center.appendChild(close);
+    
+    sumnode = document.createTextNode("  " + sum);
+    center.appendChild(sumnode);
 }
